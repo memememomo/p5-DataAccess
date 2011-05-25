669e59fa95efe783e80ee3bef93072fbd5cb1129
memememomo <memememomo@gmail.com>
2011-05-25 20:39:56 +0900

    initial commit

---
+++ lib/DataAccess.pm
@@ -0,0 +1,32 @@
+package DataAccess;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DataAccess -
+
+=head1 SYNOPSIS
+
+  use DataAccess;
+
+=head1 DESCRIPTION
+
+DataAccess is
+
+=head1 AUTHOR
+
+memememomo E<lt>memememomo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
